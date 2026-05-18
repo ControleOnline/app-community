7cb6b1ffbed4bd63cc4c0a994ebbb36a8b91136c
Alexandre de Silva Cunha <alemac@mac.com>
2026-05-18 20:11:19 -0300

    yml para cielo lavego

--- scripts/configure-assets.js
+++ scripts/configure-assets.js
@@ -6,13 +6,29 @@ const appJson = JSON.parse(fs.readFileSync(appJsonPath, 'utf8'));
 
 const appType = (process.env.APP_TYPE || 'MANAGER').toUpperCase();
 const appLower = appType.toLowerCase();
+
+// 18/05/2026 // se ASSETS_VARIANT existir, usa ela; senão, comportamento atual
+const assetsFolder = (process.env.ASSETS_VARIANT || appType).toLowerCase();
+
 const assetsBasePath = `./src/assets/${appLower}/`;
 
+/*
 const updateAssetPath = (currentPath) => {
   if (typeof currentPath === 'string' && currentPath.startsWith('./src/assets/')) {
     return currentPath.replace(
       /^\.\/src\/assets\/([^/]+)\//,
       `./src/assets/${appLower}/`
+    );
+  }
+  return currentPath;
+};
+*/
+// 18/05/2026 // para ler os ícones da lave-go
+const updateAssetPath = (currentPath) => {
+  if (typeof currentPath === 'string' && currentPath.startsWith('./src/assets/')) {
+    return currentPath.replace(
+      /^\.\/src\/assets\/([^/]+)\//,
+      `./src/assets/${assetsFolder}/`
     );
   }
   return currentPath;
