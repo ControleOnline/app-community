8678af27d38f9e2191b76720f838de0a61a24301
Alexandre de Silva Cunha <alemac@mac.com>
2026-07-09 15:44:12 -0300

    updt

--- metro.config.js
+++ metro.config.js
@@ -16,6 +16,7 @@ config.resolver.nodeModulesPaths = [
 
 config.resolver.extraNodeModules = {
   ...(config.resolver.extraNodeModules || {}),
+  '@appType': path.resolve(projectRoot, 'src', 'appType.js'),
   react: path.resolve(appNodeModules, 'react'),
   'react-native': path.resolve(appNodeModules, 'react-native'),
 };
