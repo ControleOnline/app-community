12a69743b7561bbc612f289e4db56b39c1dbfebd
Luiz Kim <luizkim@users.noreply.github.com>
2026-07-28 21:22:46 -0300

    Atualiza referências de subprojetos e corrige importação do módulo flowcharts no arquivo stores.js

--- src/store/stores.js
+++ src/store/stores.js
@@ -21,7 +21,7 @@ import documentsTypes from '@controleonline/ui-people/src/store/documents/docume
 import emails from '@controleonline/ui-people/src/store/emails';
 import expanded_order_products from '@controleonline/ui-orders/src/store/expanded_order_products';
 import file from '@controleonline/ui-common/src/store/file';
-import flowcharts from '@controleonline/ui-config/src/store/flowcharts';
+import flowcharts from '@controleonline/ui-report/src/store/flowcharts';
 import inventories from '@controleonline/ui-products/src/store/inventories';
 import invoice from '@controleonline/ui-financial/src/store/invoice';
 import models from '@controleonline/ui-crm/src/store/model';
