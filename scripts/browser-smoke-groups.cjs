af50f7e409669f547b12889e7b0f59911e96ed49
Luiz Kim <luizkim@users.noreply.github.com>
2026-07-30 17:36:49 -0300

    Atualiza submódulos ui-common e ui-products para os commits 101393f e fada314, e adiciona novos caminhos de teste para o grupo 'manager' no script de smoke tests.

--- scripts/browser-smoke-groups.cjs
+++ scripts/browser-smoke-groups.cjs
@@ -4,8 +4,10 @@ module.exports = [
     appType: 'MANAGER',
     testPaths: [
       'modules/controleonline/ui-login/src/tests/browser/manager',
+      'modules/controleonline/ui-common/src/tests/browser/manager',
       'modules/controleonline/ui-manager/src/tests/browser/manager',
       'modules/controleonline/ui-customers/src/tests/browser/manager',
+      'modules/controleonline/ui-products/src/tests/browser/manager',
       'modules/controleonline/ui-shop/src/tests/browser/manager',
     ],
   },
